1ee026ce6b32282297f07a8acb845a0814ed7e33
Kyle Chu <kylewchu@gmail.com>
2022-04-28 15:43:41 -0700

    245 Add Coaches to Vuex

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,3 +1,15 @@
 export default {
-
+  registerCoach(context, data) {
+    const coachData = {
+      id: context.rootGetters.userId,
+      firstName: data.first,
+      lastName: data.last,
+      description: data.desc,
+      hourlyRate: data.rate,
+      areas: data.areas
+    };
+    console.log("HELLO");
+    console.log(coachData.id);
+    context.commit('registerCoach', coachData);
+  }
 };
--- src/store/modules/coaches/getters.js
+++ src/store/modules/coaches/getters.js
@@ -4,6 +4,12 @@ export default {
   },
   hasCoaches(state) {
     return state.coaches && state.coaches.length > 0;
+  },
+  isCoach(_, getters, _2, rootGetters) {
+    const coaches = getters.coaches;
+    const userId = rootGetters.userId;
+    console.log(coaches);
+    return coaches.some(coach => coach.id === userId);
   }
 };
 
