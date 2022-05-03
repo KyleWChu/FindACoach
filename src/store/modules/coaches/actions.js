12bacb0bce7911b419a17d8d8a4964397942ed22
Kyle Chu <kylewchu@gmail.com>
2022-05-03 13:40:34 -0700

    Thru 254 Error handling

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -6,24 +6,55 @@ export default {
       lastName: data.last,
       description: data.desc,
       hourlyRate: data.rate,
-      areas: data.areas
+      areas: data.areas,
     };
 
     //251 Send a PUT Request, fetch returns a promise
-    const response = await fetch(`https://coach-project-f3806-default-rtdb.firebaseio.com/coaches/${userId}.json`, {
-      method: 'PUT',
-      body: JSON.stringify(coachData)
-    });
+    const response = await fetch(
+      `https://coach-project-f3806-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData),
+      }
+    );
 
     // const responseData = await response.json();
 
-    if(!response.ok) {
+    if (!response.ok) {
       //error
     }
 
     context.commit('registerCoach', {
       ...coachData,
-      id: userId
+      id: userId,
     });
-  }
+  },
+  async loadCoaches(context) {
+    //252 Fetching Coaches
+    const response = await fetch(
+      `https://coach-project-f3806-default-rtdb.firebaseio.com/coaches.json`
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Failed to fetch!');
+      throw error;
+    }
+
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+
+    context.commit('setCoaches', coaches);
+  },
 };
